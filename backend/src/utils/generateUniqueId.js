747e22d54870d52aa3b310f59eec9d230122c295
André Euzébio <andre.r.euzebio@gmail.com>
2020-03-29 20:29:43 -0300

    Correção de bug relacionado a verificação da ID unica ao inserir ONGs novas

--- backend/src/utils/generateUniqueId.js
+++ backend/src/utils/generateUniqueId.js
@@ -1,21 +1,22 @@
 const crypto = require('crypto');
 const connection = require('../database/connection');
 
-let id = 0;
-const generateId = async () => {
-    let existentId = true;
-    while(existentId){
-        id = crypto.randomBytes(4).toString('HEX');
-        existentId = await connection('ongs')
-                                .where('id',id)            
-                                .select('id')
-                                .first();
-    }     
-    return id
-}
 
-generateId();
 
 module.exports = function gerenarteUniqueId() {
-    return id;
+    let id = crypto.randomBytes(4).toString('HEX');
+    async function generateId() {
+        let existentId = true;
+        while(existentId){
+            id = crypto.randomBytes(4).toString('HEX');
+            existentId = await connection('ongs')
+                                    .where('id',id)            
+                                    .select('id')
+                                    .first();
+        }     
+        console.log(id);
+        return id
+    }
+    
+    return generateId();
 }
